670b9ca376b556f20676867e6800524d252196c2
Nathan Keller <keller.nathan@gmail.com>
2020-09-14 13:30:42 -0600

    Adjust verbage on Recipe page

--- app/javascript/components/Recipes.jsx
+++ app/javascript/components/Recipes.jsx
@@ -52,10 +52,9 @@ class Recipes extends React.Component {
       <>
         <section className="jumbotron jumbotron-fluid text-center">
           <div className="container py-5">
-            <h1 className="display-4">Recipes for every occasion</h1>
+            <h1 className="display-4">All The Best Recipes</h1>
             <p className="lead text-muted">
-              We’ve pulled together our most popular recipes, our latest
-              additions, and our editor’s picks, so there’s sure to be something
+              A list of my most popular recipes. There’s sure to be something
               tempting for you to try.
             </p>
           </div>
