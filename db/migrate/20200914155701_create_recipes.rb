7c66254e820af777a9ddefb18687ce232bd16f40
Nathan Keller <keller.nathan@gmail.com>
2020-09-14 10:17:05 -0600

    Add defualt image to Recipe Model

--- db/migrate/20200914155701_create_recipes.rb
+++ db/migrate/20200914155701_create_recipes.rb
@@ -1,10 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration[5.2]
   def change
     create_table :recipes do |t|
-      t.string :name
-      t.text :ingredients
-      t.text :instruction
-      t.string :image
+      t.string :name, null: false
+      t.text :ingredients, null: false
+      t.text :instruction, null: false
+      t.string :image, defualt: 'https://cdn.pixabay.com/photo/2014/06/11/17/00/cook-366875__340.jpg'
 
       t.timestamps
     end
