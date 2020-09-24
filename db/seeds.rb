f93f0f4149e59c5e7f89f9d7352121d7f617e119
Nathan Keller <keller.nathan@gmail.com>
2020-09-24 09:53:39 -0600

    Add new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-9.times do |i|
-  Recipe.create(
-    name: "Recipe #{i + 1}",
-    ingredients: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
-    instruction: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat and bring to a low simmer. Turn off the heat and keep warm.'
-  )
-end
+Recipe.create(
+  name: "Carne Asada",
+  ingredients: "1/2 C chopped cilantro, 1/3 C Olive Oil, 1/4 Soy Sauce, Juice-1 Orange, Juice-1 Lime, 4 Cloves Garlic, 1 Jalapeno, 1 teaspoon ground cumin, salt and pepper to taste, 1 1/2 lb flank steak",
+  instruction: "1. In a medium bowl, combine cilantro, olive oil, soy sauce, orange juice, lime juice, garlic, jalapeno, cumin and 1 teaspoon pepper; set aside 1/2 cup of the mixture in the refrigerator until ready to serve.
+2. In a gallon size Ziploc bag or large bowl, combine steak and remaining cilantro mixture; marinate for at least 4 hours to overnight, turning the bag occasionally. Drain the steak from the marinade.
+3. Preheat grill to medium high heat. Using paper towels, pat both sides of the steak dry; season with salt and pepper, to taste.
+4. Add steak to grill, and cook, flipping once, until desired doneness, about 6 minutes per side for medium rare. Let rest 5 minutes.
+5. Thinly slice steak against the grain and serve with reserved 1/2 cup cilantro mixture.",
+  image: "https://s23209.pcdn.co/wp-content/uploads/2019/05/Carne-AsadaIMG_8244.jpg"
+)
